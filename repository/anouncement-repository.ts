737ab2f8cc6d35a9edc200f7a475721cabec42aa
Hongyi Syue <homeyxue@outlook.com>
2020-04-01 22:01:02 -0600

    modify anouncement repo

--- repository/anouncement-repository.ts
+++ repository/anouncement-repository.ts
@@ -2,25 +2,13 @@ import { Injectable } from "https://deno.land/x/alosaur/src/mod.ts";
 import { ObjectId } from "https://deno.land/x/mongo@v0.4.0/ts/types.ts";
 import { RepoConnection } from "./repoConnection.ts";
 import { Anouncement } from "../domain-model/anouncement.ts";
+import { Repository } from "./repository.ts";
 
 @Injectable()
-export class AnouncementRepository {
-    readonly CollectionName = "anouncement";
+export class AnouncementRepository extends Repository<Anouncement>{
 
-    get collection() {
-        return this.repository.database?.collection(this.CollectionName);
-    }
-
-    constructor(private repository: RepoConnection) { }
-
-    async addSingle(menuCategory: Anouncement) {
-        return await this.collection?.insertOne(menuCategory);
-    }
-
-    async getSingle(id: string) {
-        return this.getMenuCategoryInstanceWithId(
-            await this.collection?.findOne({ _id: ObjectId(id) }) as Anouncement
-        );
+    constructor(private repoConnection: RepoConnection) {
+        super(repoConnection, "anouncements");
     }
 
     async getAllIds() {
@@ -32,21 +20,9 @@ export class AnouncementRepository {
         return menuCategoryIdList;
     }
 
-    async modify(id: string, changDefinition: any) {
-        return await this.collection?.updateOne(
-            { _id: ObjectId(id) },
-            { $set: changDefinition }
-        );
-    }
-
     async delete(id: string) {
         return await this.collection?.deleteOne(
             { _id: ObjectId(id) }
         );
-    }
-
-    private getMenuCategoryInstanceWithId(menuCategory: Anouncement) {
-        menuCategory.id = (menuCategory as any)._id.$oid;
-        return menuCategory;
     }
 }
