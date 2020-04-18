d28dc07cca01dac60e4086e2d57a35638e3dfcee
Sean Wong <seanwong24@live.com>
2020-04-18 22:32:19 +0000

    modify import for repository connection

--- repository/repository-connection.ts
+++ repository/repository-connection.ts
@@ -1,16 +1,16 @@
-import * as mongo from "https://deno.land/x/mongo/mod.ts";
+import { MongoClient, Database, init } from "https://deno.land/x/mongo/mod.ts";
 
 export class RepositoryConnection {
-  client: mongo.MongoClient | undefined;
-  database: mongo.Database | undefined;
+  client: MongoClient | undefined;
+  database: Database | undefined;
 
   constructor(connectionString: string, databaseName: string) {
     this.connect(connectionString, databaseName);
   }
 
   async connect(connectionString: string, databaseName: string) {
-    await mongo.init();
-    const client = new mongo.MongoClient();
+    await init();
+    const client = new MongoClient();
     client.connectWithUri(connectionString);
 
     this.client = client;
